03e1d8f31db9c0eb5d7717be91c09e31a3826d8f
Arnex93 <arnaud.du.93@gmail.com>
2016-06-16 16:51:58 +0200

    Design of pop-up ended

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,2 +1,15 @@
 
 // This will get executed when clicking on icon
+
+var mails = 4;
+
+var meteo = 150;
+$( ".meteo" ).html("Il fait actuellement " + meteo + "°C");
+
+if (mails == 0) {
+    $( ".mails" ).html("Vous n'avez pas de nouveau mail");
+}else if (mails == 1){
+    $( ".mails" ).html("Vous avez 1 nouveau mail");
+}else{
+    $( ".mails" ).html("Vous avez " + mails + " nouveaux mails");
+}
